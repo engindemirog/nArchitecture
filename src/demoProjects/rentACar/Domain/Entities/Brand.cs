fc8fb39bf69b604cb69468fb4c8997af492065af
HBA114 <hbasriayhaner114@gmail.com>
2022-08-15 22:32:54 +0300

    initial Commit

---
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,25 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Brand : Entity
+    {
+        public string Name { get; set; }
+
+        public Brand()
+        {
+
+        }
+
+        public Brand(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
