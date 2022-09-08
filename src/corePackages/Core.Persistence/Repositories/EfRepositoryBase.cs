3e74215486664885e40e038294f448bd407cb708
MURAT AĞYÜZ <182804010@ogr.cbu.edu.tr>
2022-09-08 17:55:40 +0300

    GetAsync method add include parameter

--- src/corePackages/Core.Persistence/Repositories/EfRepositoryBase.cs
+++ src/corePackages/Core.Persistence/Repositories/EfRepositoryBase.cs
@@ -17,9 +17,12 @@ public class EfRepositoryBase<TEntity, TContext> : IAsyncRepository<TEntity>, IR
         Context = context;
     }
 
-    public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate)
+    public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null)
     {
         return await Context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+        IQueryable<TEntity> queryable = Context.Set<TEntity>();
+        if (include != null) queryable = include(queryable);
+        return await queryable.FirstOrDefaultAsync(predicate);
     }
 
     public async Task<IPaginate<TEntity>> GetListAsync(Expression<Func<TEntity, bool>>? predicate = null,
