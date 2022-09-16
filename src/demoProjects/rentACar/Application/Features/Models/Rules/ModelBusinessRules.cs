26f9877bd7eb11492105cf03b0cb429e8691f928
HBA114 <hbasriayhaner114@gmail.com>
2022-09-16 17:08:16 +0300

    Created launch.json and tasks.json for vscode

---
+++ src/demoProjects/rentACar/Application/Features/Models/Rules/ModelBusinessRules.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services.Repositories;
+
+namespace Application.Features.Models.Rules
+{
+    public class ModelBusinessRules
+    {
+        private readonly IModelRepository _modelRepository;
+
+        public ModelBusinessRules(IModelRepository modelRepository)
+        {
+            _modelRepository = modelRepository;
+        }
+    }
+}
